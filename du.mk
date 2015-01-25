3f6db41d6c0ead3086e2654777a93425ad32a9cc
chasmodo <bosko.trkulja@gmail.com>
2015-01-25 22:41:41 +0100

    Initial DU bringup
    Change-Id: Idf4ec24733f1abc04fbb07490c3bb3315dfe8eaf

--- du.mk
+++ du.mk
@@ -1,17 +1,13 @@
-# Torch
-PRODUCT_PACKAGES := \
-    OmniTorch
-
 # Inherit Omni GSM telephony parts
-$(call inherit-product, vendor/omni/config/gsm.mk)
+$(call inherit-product, vendor/du/config/gsm.mk)
 
 # Inherit Omni product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/du/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/sony/yuga/full_yuga.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6603 BUILD_FINGERPRINT=Sony/C6603/C6603:4.4.4/10.5.1.A.0.283/5P53rQ:user/release-keys PRIVATE_BUILD_DESC="C6603-user 4.4.4 10.5.1.A.0.283 5P53rQ release-keys"
 
-PRODUCT_NAME := omni_yuga
+PRODUCT_NAME := du_yuga
 PRODUCT_DEVICE := yuga
